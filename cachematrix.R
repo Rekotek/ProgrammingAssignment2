375e0a692ec08c56b54783af502616165db6ecbb
Taras Tkachenko <TarasTkachenko@me.com>
2015-01-27 11:37:38 +0200

    Working with cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a matrix object with getter and setter for the matrix
+## and getter for inverse form, which will be calculated and cached
+## after first request 
 
 makeCacheMatrix <- function(x = matrix()) {
+  invMatrix <- NULL
+  
+  get <- function() x
+  
+  set <- function(m) {
+    x <<- m
+    invMatrix <<- NULL
+  }
+    
+  getInv <- function() {
+    if (is.null(invMatrix))
+      invMatrix <- solve(x)
 
+    invMatrix
+  }
+  
+  list(get = get, set = set, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Just return a value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  x$getInv()
 }
